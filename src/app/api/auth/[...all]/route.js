2ed6a9e7973255c6f3ee858bb960c4eb439e7d43
Nezamkha32 <nezamkha32@gmail.com>
2026-05-03 13:19:53 +0600

    add my profile page and update

--- src/app/api/auth/[...all]/route.js
+++ src/app/api/auth/[...all]/route.js
@@ -2,5 +2,6 @@ import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
 const handler = toNextJsHandler(auth);
-console.log(auth);
-export const { GET, POST } = toNextJsHandler(auth);
+
+export const GET = handler.GET;
+export const POST = handler.POST;
