71ad1b7290639b8a4b074043a506ba61de59ee0b
Nezamkha32 <nezamkha32@gmail.com>
2026-05-03 13:04:19 +0600

    add my profile page and update information

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,3 +3,4 @@ export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: "https://a8-sun-cart-summer-essentials-store.vercel.app"
 })
+export const { signIn, signUp, useSession } = createAuthClient()
